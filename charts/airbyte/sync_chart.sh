198d5cb9eddf1c514e1e8943edfc17ca15db803a
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2023-11-24 22:06:24 +0100

    rewrote to include hpa

---
+++ charts/airbyte/sync_chart.sh
@@ -0,0 +1,1 @@
+helm dependency update && helm dependency build && helm package . && mv airbyte-0.50.2.tgz airbyte/ && helm repo index airbyte --url https://core-ops-charts.storage.googleapis.com/airbyte &&  gsutil rsync airbyte/ gs://core-ops-charts/airbyte/
